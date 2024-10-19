e4bb0dbb705d94da016db10b2c0f7bd810af1cf7
hami9m722 <hami9m72@gmail.com>
2024-10-19 09:39:36 +0700

    fix order, user, project

--- src/main/java/tradingcarbon/my_app/repos/ProjectRepository.java
+++ src/main/java/tradingcarbon/my_app/repos/ProjectRepository.java
@@ -3,6 +3,7 @@ package tradingcarbon.my_app.repos;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tradingcarbon.my_app.domain.Project;
+import tradingcarbon.my_app.domain.ReviewProject;
 import tradingcarbon.my_app.domain.User;
 
 
