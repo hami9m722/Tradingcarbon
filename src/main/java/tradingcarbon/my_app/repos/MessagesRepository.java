d00b6386d6be404b6788bae0ad46bce30942d4ea
nhile14112003 <21522425@gm.uit.edu.vn>
2024-10-25 12:15:27 +0700

    add messing for chat fix sth in review

---
+++ src/main/java/tradingcarbon/my_app/repos/MessagesRepository.java
@@ -0,0 +1,11 @@
+package tradingcarbon.my_app.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tradingcarbon.my_app.domain.Messages;
+import tradingcarbon.my_app.domain.User;
+
+
+public interface MessagesRepository extends JpaRepository<Messages, Long> {
+
+
+}
