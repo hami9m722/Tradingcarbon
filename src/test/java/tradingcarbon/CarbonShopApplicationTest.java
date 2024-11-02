f8e5b783774615c39428a1ba7aa07d728c21fca2
hami9m722 <hami9m72@gmail.com>
2024-11-03 05:05:20 +0700

    set up test

---
+++ src/test/java/tradingcarbon/CarbonShopApplicationTest.java
@@ -0,0 +1,14 @@
+package tradingcarbon;
+
+import org.junit.jupiter.api.Test;
+
+import tradingcarbon.my_app.config.BaseIT;
+
+
+public class CarbonShopApplicationTest extends BaseIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
